07b02a9516666dfd4969daf26cac642a9c2ab540
Adetokunbo Ogunnoiki <adetokunboogunnoiki28@gmail.com>
2025-01-27 22:48:15 +0100

    fix : renamed component cause of Next.js restrictions

--- src/app/_global-components/icons/index.ts
+++ src/app/_global-components/icons/index.ts
@@ -1,7 +1,7 @@
 export {default as BoltIcon} from "./bolt";
 export {default as StarIcon} from "./star";
 export {default as BoostIcon} from "./boost";
-export {default as RouteIcon} from "./route";
+export {default as RouteIcon} from "./routeIcon";
 export {default as ToolsIcon} from "./tools";
 export {default as TrackIcon} from "./track";
 export {default as MegaPhoneIcon} from "./megaphone";

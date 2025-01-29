2009437234cb0207f0d0ed4505bc5be9d3d5a0e7
Adetokunbo Ogunnoiki <adetokunboogunnoiki28@gmail.com>
2025-01-29 20:19:19 +0100

    fix: trying to figure the favicon

--- src/app/_global-components/icons/index.ts
+++ src/app/_global-components/icons/index.ts
@@ -1,8 +1,10 @@
-export {default as BoltIcon} from "./bolt";
-export {default as StarIcon} from "./star";
-export {default as BoostIcon} from "./boost";
-export {default as RouteIcon} from "./routeIcon";
-export {default as ToolsIcon} from "./tools";
-export {default as TrackIcon} from "./track";
-export {default as MegaPhoneIcon} from "./megaphone";
-export {default as ConnectIcon} from "./connect";
+export { default as BoltIcon } from "./bolt";
+export { default as StarIcon } from "./star";
+export { default as BoostIcon } from "./boost";
+export { default as RouteIcon } from "./routeIcon";
+export { default as ToolsIcon } from "./tools";
+export { default as TrackIcon } from "./track";
+export { default as MegaPhoneIcon } from "./megaphone";
+export { default as ConnectIcon } from "./connect";
+// export { default as FavIcon } from "./favicon";
+export const FavIcon = '/favicon.ico'; // Path to the favicon in public directory
